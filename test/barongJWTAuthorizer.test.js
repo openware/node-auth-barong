16e1738f4f7732eefa286e0f92c1a8d230d90197
alinetskyi <artem.linetskiy@gmail.com>
2019-08-12 17:19:57 +0300

    Feature: Implement options validations and tests

--- test/barongJWTAuthorizer.test.js
+++ test/barongJWTAuthorizer.test.js
@@ -2,18 +2,27 @@
  * Dependencies.
  */
 
-var barongJWTAuthorizer = require("../lib/barongJWTAuthorizer");
-var assert = require("assert");
-var mocha = require("mocha");
-var describe = mocha.describe;
-var it = mocha.it;
+var barongJWTAuthorizer = require("../lib/barongJWTAuthorizer")
+var assert = require("assert")
+var mocha = require("mocha")
+var expect = require("chai").expect
+var describe = mocha.describe
+var it = mocha.it
 
 /**
  * Tests.
  */
 
-describe("barongJWTAuthorizer()", function() {
-  it("should be a function", function() {
-    assert.strictEqual(typeof barongJWTAuthorizer, "function");
-  });
-});
+describe("barongJWTAuthorizer()", function () {
+  it("should be a function", function () {
+    assert.strictEqual(typeof barongJWTAuthorizer, "function")
+  })
+
+  it("should raise error when Barong JWT Public key is not set", function () {
+    expect(() => barongJWTAuthorizer({})).to.throw("Barong JWT Pulic key should be set")
+  })
+
+  it("should raise error when barong public", function () {
+    expect(() => barongJWTAuthorizer({ barongJwtPublicKey: "pub_key" })).to.throw("Application's private key should be set")
+  })
+})
